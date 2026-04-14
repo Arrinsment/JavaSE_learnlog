b0b969045fdc08b1f1df9d31fe4611c86f40098c
RiverWanwan <2432888586@qq.com>
2026-04-14 09:52:09 +0800

    第二天的代码

---
+++ day02-code/src/com/itheima/demo1/HelloWorld2.java
@@ -0,0 +1,11 @@
+package com.itheima.demo1;
+
+import java.util.Scanner;
+
+public class HelloWorld2 {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int a = sc.nextInt();
+
+    }
+}
