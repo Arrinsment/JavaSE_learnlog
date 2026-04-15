34a94c4481d73981e1a8f0c73ca480c1c8d0d2c4
RiverWanwan <2432888586@qq.com>
2026-04-15 21:01:05 +0800

    new log

---
+++ Day21(泛型及其集合)/src/SetDemo1/Student.java
@@ -0,0 +1,57 @@
+package SetDemo1;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private  int age;
+
+
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+ //这里的这个 从写的这个equals 方法是比较2个对象的内容是不是相同,然后比较的不是地址值了
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
