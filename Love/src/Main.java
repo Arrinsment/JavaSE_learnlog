8d10501a2f9843fbcbfbff317cae5f670866c43a
RiverWanwan <2432888586@qq.com>
2026-04-21 15:21:55 +0800

    斗地主1.0

--- Love/src/Main.java
+++ Love/src/Main.java
@@ -1,18 +1,36 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-     Scanner SC=new Scanner(System.in);
-     int a=SC.nextInt();
-     int b=SC.nextInt();
-     int c=SC.nextInt();
+public class Main{
 
-     int getApple=(int) Math.ceil((double)(c/b));
-     if(b==0){
-         System.out.println(0);
-     }
-     else{
-         System.out.println(a-getApple);
-     }
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        ArrayList<Integer>list1=new ArrayList<>();
+        ArrayList<Integer>list2=new ArrayList<>();
+      for (int i=0;i<7;i++){
+          int a=sc.nextInt();
+          int b=sc.nextInt();
+          list1.add(a);
+          list2.add(b);
+      }
+
+      int max=8;int index =0;
+        for (int i = 0; i < 7; i++) {
+            if((list1.get(i)+list2.get(i))>max){
+                max=list1.get(i)+list2.get(i);
+                index=i;
+            }
+        }
+
+        if (max==8){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(index+1);
+        }
+
+
     }
 }
